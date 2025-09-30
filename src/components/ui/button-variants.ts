d3c140d6d5e8c362e84eabeaba0be1163162914d
Harold Park <hyungkip@gmail.com>
2025-09-30 13:21:26 -0700

    feat: button changes

--- src/components/ui/button-variants.ts
+++ src/components/ui/button-variants.ts
@@ -18,7 +18,7 @@ export const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-12 px-4 py-2 has-[>svg]:px-3',
+        default: 'h-6 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-11 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
